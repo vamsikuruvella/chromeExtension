334daa1f51f4ea728ffeb7e3dfb0766d5c619877
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-03-01 05:17:10 +0000

    chrome extension

---
+++ popup.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var changeColorButton = document.getElementById('changeColor');
+  
+    changeColorButton.addEventListener('click', function () {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+          tabs[0].id,
+          { code: 'document.body.style.backgroundColor = "lightblue";' }
+        );
+      });
+    });
+  });
+  
