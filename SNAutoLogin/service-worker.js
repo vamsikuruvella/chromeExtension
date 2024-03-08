bd11b56b71c30626a5fd7f9aa83a22bfdfb3bc5f
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-03-08 06:09:48 +0000

    gg

--- SNAutoLogin/service-worker.js
+++ SNAutoLogin/service-worker.js
@@ -6,12 +6,28 @@ function reddenPage() {
     console.log(urll);
     if(urll.includes("https://developer.servicenow.com/dev.do")){
       window.location.href="https://signon.service-now.com/x_snc_sso_auth.do?pageId=username";
-      document.getElementById("email").value="kuruvellasaivamsi@gmail.com";
-      document.getElementById("username_submit_button").click();
+
     }
-    else if(urll.includes("https://signon.service-now.com/x_snc_sso_auth.do?pageId=password")){
-      document.getElementById("password").value="Santhu@1234";
-      document.getElementById("password_submit_button").click();
+else if(urll.includes("https://signon.service-now.com/x_snc_sso_auth.do?pageId=username")){
+      //document.getElementById("password").value="Santhu@1234";
+      //setTimeout(resolveInner, 100);
+if(document.getElementById("email").value==""){
+      document.getElementById("email").value="kuruvellasaivamsi1@gmail.com";
+      }
+      setTimeout(function() {
+        document.getElementById("username_submit_button").click();
+    }, 500);
+    }    
+
+else if(urll.includes("https://signon.service-now.com/x_snc_sso_auth.do?pageId=password")){
+      //document.getElementById("password").value="Santhu@1234";
+      //setTimeout(resolveInner, 100);
+if(document.getElementById("password").value==""){
+      document.getElementById("password").value="Sri@2kanth";
+      }
+      setTimeout(function() {
+        document.getElementById("password_submit_button").click();
+    }, 500);
     }
   }
   
