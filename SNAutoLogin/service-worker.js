926fabfc86382861f6ae0b1e5753e67a5cb90ac3
vamsikuruvella <90389710+vamsikuruvella@users.noreply.github.com>
2024-03-08 05:23:07 +0000

    gg

--- SNAutoLogin/service-worker.js
+++ SNAutoLogin/service-worker.js
@@ -1,5 +1,18 @@
+console.log("ReddenPage Line 1");
 function reddenPage() {
-    document.body.style.backgroundColor = 'red';
+    //console.log("ReddenPage Line 2");
+    //document.body.style.backgroundColor = 'red';
+    const urll=window.location.href;
+    console.log(urll);
+    if(urll.includes("https://developer.servicenow.com/dev.do")){
+      window.location.href="https://signon.service-now.com/x_snc_sso_auth.do?pageId=username";
+      document.getElementById("email").value="kuruvellasaivamsi@gmail.com";
+      document.getElementById("username_submit_button").click();
+    }
+    else if(urll.includes("https://signon.service-now.com/x_snc_sso_auth.do?pageId=password")){
+      document.getElementById("password").value="Santhu@1234";
+      document.getElementById("password_submit_button").click();
+    }
   }
   
   chrome.action.onClicked.addListener((tab) => {
